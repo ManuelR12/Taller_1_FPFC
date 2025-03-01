dc2766111899fba49620504b03ebf8a3f2df3d46
Manuel Rodriguez <juan.rodriguez.o@correounivalle.edu.co>
2025-03-01 15:05:25 -0500

    last version

--- src/main/scala/Multiplicacion/package.scala
+++ src/main/scala/Multiplicacion/package.scala
@@ -1,33 +1,32 @@
 package object Multiplicacion {
 
-
   def PeasantAlgorithm(a: Int, b: Int): Int = {
     if (a == 0) 0
     else if (a % 2 == 0) PeasantAlgorithm(a / 2, b + b)
     else PeasantAlgorithm(a / 2, b + b) + b
   }
 
-  def PeasantAlgorithmIt(a: Int, b: Int, count: Int): Int = {
-    if (a == 0) count
-    else if (a % 2 == 0) PeasantAlgorithmIt(a / 2, b + b, count)
-    else PeasantAlgorithmIt(a / 2, b + b, count + b)
+  def PeasantAlgorithmIt(a: Int, b: Int): Int = {
+    def helper(a: Int, b: Int, acc: Int): Int = {
+      if (a == 0) acc
+      else if (a == 1) acc + b
+      else if (a % 2 == 0) helper(a / 2, b + b, acc)
+      else helper(a / 2, b + b, acc + b)
+    }
+    helper(a, b, 0)
   }
 
-  def PeasantItCall(a: Int, b: Int): Int = { PeasantAlgorithmIt(a, b, 0)}
-
   def splitMultiply(a: Int, b: Int): Int = {
-    if (a < 10 || b < 10) a * b
+    if (a < 10 && b < 10) a * b
 
     else{
-      val m = (math.max(a.toString.length, b.toString.length) + 1) / 2
-      val divider = math.pow(10, m).toInt
+      val m = math.max(a.toString.length, b.toString.length) / 2
 
-      val (x, y) = (a / divider, a % divider)
-      val (z, w) = (b / divider, b % divider)
+      val (x, y) = (a / math.pow(10, m).toInt, a % math.pow(10, m).toInt)
+      val (z, w) = (b / math.pow(10, m).toInt, b % math.pow(10, m).toInt)
 
       val p1 = splitMultiply(x, z)
       val p2 = splitMultiply(y, w)
-
       val p3 = splitMultiply(x,w) + splitMultiply(y,z)
 
       println(s"a=$a, b=$b, m=$m, x=$x, y=$y, z=$z, w=$w, p1=$p1, p2=$p2, p3=$p3")
@@ -35,6 +34,26 @@ package object Multiplicacion {
       p1 * math.pow(10, 2 * m).toInt + p3 * math.pow(10, m).toInt + p2
     }
   }
+
+  def fastMultiply(a: Int, b: Int): Int = {
+    if (a < 10 && b < 10) a * b
+
+    else {
+      val m = math.max(a.toString.length, b.toString.length) / 2
+
+      val (x, y) = (a / math.pow(10, m).toInt, a % math.pow(10, m).toInt)
+      val (z, w) = (b / math.pow(10, m).toInt, b % math.pow(10, m).toInt)
+
+      val p1 = fastMultiply(x, z)
+      val p2 = fastMultiply(y, w)
+      val p3 = (p1 + p2) - fastMultiply(x - y, z - w)
+
+      println(s"a=$a, b=$b, m=$m, x=$x, y=$y, z=$z, w=$w, p1=$p1, p2=$p2, p3=$p3")
+
+      p1 * math.pow(10, 2 * m).toInt + p3 * math.pow(10, m).toInt + p2
+    }
+  }
+
 }
 
   
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -6,9 +6,8 @@ println(PeasantAlgorithm(7, 3))  // 21
 println(PeasantAlgorithm(8, 4))  // 32
 println(PeasantAlgorithm(2145, 3233))  // 6926985
 println(PeasantAlgorithm(12, 11))  // 132
-
-println(PeasantItCall(2, 5))
+println(PeasantAlgorithmIt(13, 5))
 
 // Pruebas para SplitAlgorithm
-println(splitMultiply(10, 323))  // 20
-
+println(splitMultiply(32484, 47382))  // 20
+println(fastMultiply(324843, 1000))
